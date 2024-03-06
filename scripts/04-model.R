52062e969e98a2b778cbc4f74d9ece80031c871e
Yimiao Yuan <yymlinda.yuan@mail.utoronto.ca>
2024-03-05 20:04:36 -0500

    Build model explaining vote based on education and gender

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -11,16 +11,23 @@
 #### Workspace setup ####
 library(tidyverse)
 library(rstanarm)
+library(arrow)
 
 #### Read data ####
-analysis_data <- read_csv("outputs/data/analysis_data.csv")
+analysis_data <- read_parquet("data/analysis_data/analysis_data.parquet")
+
+set.seed(853)
+
+analysis_data <- 
+  ces2020 |> 
+  slice_sample(n = 1000)
 
 ### Model data ####
 first_model <-
   stan_glm(
-    formula = flying_time ~ length + width,
+    formula = voted_for ~ gender + education,
     data = analysis_data,
-    family = gaussian(),
+    family = binomial(link = "logit"),
     prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
     prior_aux = exponential(rate = 1, autoscale = TRUE),
@@ -31,7 +38,7 @@ first_model <-
 #### Save model ####
 saveRDS(
   first_model,
-  file = "outputs/models/first_model.rds"
+  file = "models/first_model.rds"
 )
 
 
